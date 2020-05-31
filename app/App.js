2606c9d0f2e3aa24d9b20a11127d69b7c26f73f8
tohid_t <tohid.taghavi@hotmail.com>
2020-05-31 11:31:46 +0430

    fix: add colorSchema

--- app/App.js
+++ app/App.js
@@ -3,11 +3,16 @@ import Navigator from 'routes';
 import {Provider as PaperProvider} from 'react-native-paper';
 import themes from 'view/style/themes';
 import {getTheme} from 'store/selectors/appearance';
-import {useSelector} from 'react-redux';
-import {StatusBar} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import {StatusBar, useColorScheme} from 'react-native';
+import {toggleTheme} from './store/reducers/appearance';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const colorSchema = useColorScheme();
   const theme = useSelector(getTheme);
+  if (colorSchema && colorSchema != theme) dispatch(toggleTheme());
+  
   useEffect(() => {
     if (theme === 'light') {
       StatusBar.setBarStyle('dark-content');
